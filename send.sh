f00ffbb07e520dfcdb4b0ff7be75f0f7462b90c3
saintsHr <hiqkrs@gmail.com>
2026-02-07 16:41:10 -0300

    commit: 5

--- send.sh
+++ send.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-COUNTER_FILE=".commit_counter"
-
-# inicializa o contador se não existir
-if [ ! -f "$COUNTER_FILE" ]; then
-  echo 0 > "$COUNTER_FILE"
-fi
-
-count=$(cat "$COUNTER_FILE")
-count=$((count + 1))
-
-echo "$count" > "$COUNTER_FILE"
-
 git add .
+count=$(git rev-list --count HEAD)
 git commit -m "commit: $count"
 git push
 clear
